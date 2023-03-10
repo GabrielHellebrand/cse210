0989ca91dd32bce897411a5ec3858ea7172a2f2b
Gabriel Hellebrand <hellebrandg@gmail.com>
2023-03-10 13:28:11 -0700

    It Don't work

--- prove/Develop05/Develop05-2/simplegoal.cs
+++ prove/Develop05/Develop05-2/simplegoal.cs
@@ -1,6 +1,6 @@
-class SimpGoal: Goal
+class SimpleGoal: Goal
 {
-       public SimpGoal()
+       public SimpleGoal()
     {
         // This allows the user to enter the name of the goal as well as the number of points the goal is worth.
 // It also allows the user to assign whether the goal is simple, a checklist goal or an eternal goal.
